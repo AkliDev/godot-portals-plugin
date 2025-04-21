a7447f1a305d87fc3a14ed6f2ce8a6084892111e
Vojtěch Struhár <vojtechstruhar@gmail.com>
2025-04-21 11:40:26 +0200

    Add screenshot functionality

--- scripts/SceneManager.gd
+++ scripts/SceneManager.gd
@@ -6,3 +6,17 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		print("Quitting to main menu")
 		get_tree().change_scene_to_file(MAIN_MENU_UID)
+	
+	if Input.is_action_just_pressed("take_screenshot"):
+		print("Screenshot!")
+		var img = get_tree().current_scene.get_viewport().get_texture().get_image()
+		
+		var dir = DirAccess.open("res://")
+		dir.make_dir("screenshots")
+		
+		var screenshots_dir = DirAccess.open("res://screenshots")
+		var index = screenshots_dir.get_files().size()
+		
+		var result = img.save_png("res://screenshots/screenshot_%d_%s.png" % [index, get_tree().current_scene.name.to_snake_case()])
+		print("Screenshot saved: %s" % error_string(result))
+		
